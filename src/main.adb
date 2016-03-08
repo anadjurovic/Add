e54ca0b9c7c09c9344b0108d2118cc4cee7b2205
ana <ana@sogilis.com>
2016-03-08 12:10:08 +0100

    Scheduler showing the computing time and the cycle time

--- src/main.adb
+++ src/main.adb
@@ -24,21 +24,27 @@ procedure Main is
       --------------------------------------------------------------------------
       File_Input, Expected : File_Type;
 
+      --------------------------------------------------------------------------
+      Input : Input_Array;
 
       --------------------------------------------------------------------------
-      Input     : Input_Array;
-
-      --------------------------------------------------------------------------
-      Output     : comp_Output;
+      Output : comp_Output;
 
       --------------------------------------------------------------------------
       Index_Task, Index_Input : Integer := 1;
-      C, D, E    : Long_Float;
-      B          : String (1 .. 1);
+      C, D, E                 : Long_Float;
+      B                       : String (1 .. 1);
+      --------------------------------------------------------------------------
+      start_time, current_time                   : Ada.Real_Time.Time;
+      elapsed_time_computing, elapsed_time_cycle : Float;
+
+      --------------------------------------------------------------------------
+      type T_array is array (0 .. 90) of Ada.Real_Time.Time;
+      Time_array : T_array;
 
    begin
 
-      -----------------------------------------------------------------------------
+      --------------------------------------------------------------------------
       Ada.Text_IO.Open (File_Input, Mode => In_File, Name => "input.csv");
 
       loop
@@ -61,7 +67,7 @@ procedure Main is
 
       Ada.Text_IO.Close (File_Input);
 
-      -----------------------------------------------------------------------------
+      --------------------------------------------------------------------------
       Ada.Text_IO.Open (Expected, Mode => In_File, Name => "expected.csv");
 
       loop
@@ -69,10 +75,38 @@ procedure Main is
          exit when Index_Task = 11;
          delay until (Clock + Waiting_Delay);
 
+         start_time := Ada.Real_Time.Clock;
+
+         Time_array (Index_Task) := start_time;
+
          comp (Input (Index_Task), Output);
          Ada.Long_Float_Text_IO.Get (Expected, E);
-         Put("     Expected : " &Long_Float'Image(E));
-         Put_Line ("  " & Boolean'Image (E = Output.Out1));
+         Put ("     Expected : " & Long_Float'Image (E));
+         Put ("  " & Boolean'Image (E = Output.Out1));
+
+         current_time := Ada.Real_Time.Clock;
+
+         elapsed_time_computing :=
+           Float (To_Duration (current_time - Time_array (Index_Task)));
+
+         Put
+           ("       Computing time : " &
+            Float'Image (elapsed_time_computing) &
+            " s");
+
+         elapsed_time_cycle :=
+           Float
+             (To_Duration
+                (Time_array (Index_Task) - Time_array (Index_Task - 1)));
+         case Index_Task is
+            when 1 =>
+               New_Line;
+            when others =>
+               Put_Line
+                 ("       Cycle time : " &
+                  Float'Image (elapsed_time_cycle) &
+                  " s");
+         end case;
          Skip_Line (Expected, 1);
          Index_Task := Index_Task + 1;
 
